597f0dbf8d2fb5c0e93e5d8843551260b130c200
thevivethakur <121496427+thevivethakur@users.noreply.github.com>
2025-06-12 18:31:23 +0530

    Add files via upload

---
+++ pdf_parser.py
@@ -0,0 +1,14 @@
+
+import frappe
+import fitz  # PyMuPDF
+
+@frappe.whitelist()
+def parse_pdf(file_url):
+    path = frappe.get_site_path('public', file_url.replace('/files/', ''))
+    doc = fitz.open(path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+
+    results = [line for line in text.splitlines() if "cement" in line.lower()]
+    return "\n".join(results)
